226a5562d9c4c46d6ecc7fd8a8358f0fe564db46
Victornfb <victornfb@outlook.com>
2022-09-16 13:08:00 -0300

    Feat<Server>: Add database

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,24 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (req, res) => {
+app.get('/games', (req, res) => {
+	return res.json({});
+});
+
+app.post('/ads', (req, res) => {
+	return res.status(201).json({});
+});
+
+app.get('/games/:id/ads', (req, res) => {
 	return res.json([
 		{ id: 1, name: 'Anúncio 1' },
 		{ id: 2, name: 'Anúncio 2' },
 		{ id: 3, name: 'Anúncio 3' },
 	]);
+});
+
+app.get('/ads/:id/discord', (req, res) => {
+	return res.json([]);
 });
 
 app.listen(3333);
